b863546149e04f1f1f7ae03a2bc6c83c9f9e2e8d
adilamanmohammed <adil.mohammed@okstate.edu>
2025-02-10 20:20:27 -0800

    `Refactor server.js to handle GET requests and add error handling for method not allowed`

--- server.js
+++ server.js
@@ -5,16 +5,29 @@ const server = http.createServer((req,res) => {
     // the below lines are used to set the response header
     // in detail we are setting the content type of the response
 
-    if (req.url === '/') { // if the request url is '/' then the below code will execute
-        res.writeHead(200, {'content-type' : 'text/html'}); // setting the response header
-        res.end('<h1>Home Page</h1>'); // sending the response
-    } else if (req.url === '/about') { // if the request url is '/about' then the below code will execute
-        res.writeHead(200, {'content-type' : 'text/html'}); // setting the response header
-        res.end('<h1>About</h1>'); // sending the response
-    } else {  // if the request url is not '/' or '/about' then the below code will execute
-        res.writeHead(404, {'content-type' : 'text/html'}); // setting the response header
-        res.end('<h1>Not found</h1>'); // sending the response
+    try {
+        // check if GET request
+        if (req.method === 'GET') {
+            if (req.url === '/') { // if the request url is '/' then the below code will execute
+                res.writeHead(200, {'content-type' : 'text/html'}); // setting the response header
+                res.end('<h1>Home Page</h1>'); // sending the response
+            } else if (req.url === '/about') { // if the request url is '/about' then the below code will execute
+                res.writeHead(200, {'content-type' : 'text/html'}); // setting the response header
+                res.end('<h1>About</h1>'); // sending the response
+            } else {  // if the request url is not '/' or '/about' then the below code will execute
+                res.writeHead(404, {'content-type' : 'text/html'}); // setting the response header
+                res.end('<h1>Not found</h1>'); // sending the response
+            }
+        } else {
+            throw new Error('Method not allowed');
+        }
+    } catch (error) {
+        // 500 is the status code for internal server error
+        res.writeHead(500, {'content-type' : 'text/plain'}); // setting the response header
+        res.end('Server Error'); // sending the response
     }
+
+    
 });
 // explain the below code
 // code will start the server and listen on port 
