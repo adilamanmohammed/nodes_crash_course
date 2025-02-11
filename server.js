9311bdf05652cf55b196fd47b4227a49c593120d
adilamanmohammed <adil.mohammed@okstate.edu>
2025-02-10 19:08:14 -0800

    `Update server.js to set response header and content`

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ import http from 'http';
 const PORT = 8000;
 
 const server = http.createServer((req,res) => {
-    res.write('Hello World!!!');
-    res.end();
+
+    // Set the response HTTP header with HTTP status and Content type
+    res.setHeader('Content-Type', 'text/html');
+
+    // h1 tag with Hello World!!! text
+    res.end('<h1>Hello World!!!</h1>');
 });
 
 server.listen(PORT, () => {
--- utils.js
+++ utils.js
@@ -1,3 +1,12 @@
 function generateRandomNumber() {
     return Math.floor(math.random() * 100)+1;
 }
+
+function celciusToFahrenheit (celsius) {
+    return celsius * 9/5 + 32;
+}
+
+module.exports = {
+    generateRandomNumber,
+    celciusToFahrenheit
+};
